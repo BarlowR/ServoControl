e11deee940de04cfe391e35e5264d49918e5a403
Rob B <Robbarlw@gmail.com>
2019-09-11 11:35:12 -0700

    Added Checksum method

--- RS485ChipTester/RS485ChipTester.ino
+++ RS485ChipTester/RS485ChipTester.ino
@@ -4,17 +4,78 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  byte cmd[6] = {0xDD, 0x1F,  0x0D, 0x3A, 0x0A, 0x8B};
+  unsigned char com[4] = {0xDA,0x1F,0x00,0x00};
+  unsigned short int ck16 = return_CRC(com);
+  Serial.print(highByte(ck16), HEX);
+  Serial.print(lowByte(ck16), HEX);
+  Serial.println("");
+
+
+  unsigned char ck16c[6] = {com, highByte(ck16), lowByte(ck16)};
+
+  Serial1.write(ck16c, 6);
+
   byte rsp[6];
+
+  char i = 0;
+  while(Serial1.available()){
+    rsp[i] = Serial1.read();
+    i++;
+  }
+  
+  for (int i = 0; i < 6; i++){
+    Serial.print(rsp[i], HEX);
+    Serial.print(" ");
+
+  }
+  Serial.println();
+  delay(200);
+  
+
+  
+}
+
+short int return_CRC(unsigned char cmd[4]){
+  unsigned short int crc = 0; // Result
+
+  char x, y;
+  crc=0xFFFF; //init value
+
+  for (x=0; x<4; x++)
+  {
+    crc = ( ( cmd[x] <<8 ) ^ crc);
+    for (y=0; y<8; y++ )
+    {
+      if ( crc & 0x8000 ){
+        crc = (crc << 1) ^ 0x8005;
+      }
+      else
+      {  
+        crc = crc << 1;
+      }
+    }
+  }
+  return crc;
+}
+
+
+void sndrcv(){
+  byte cmd[6] = {0xDD, 0x1F,  0x0D, 0x1A};
   Serial1.write(cmd,6);
+
+  byte rsp[6];
 
   int i = 0;
   while(Serial1.available()){
     rsp[i] = Serial1.read();
     i++;
   }
-  Serial.write(rsp, 6);
+  
+  for (int i = 0; i < 6; i++){
+    Serial.print(rsp[i]);
+    Serial.print(" ");
+
+  }
   Serial.println();
   delay(200);
 }
